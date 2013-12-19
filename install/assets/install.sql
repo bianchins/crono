b7adc87a7b4d5ae7902925b28156299eb769ce40
bianchins <bianchins@gmail.com>
2013-12-19 11:24:00 +0100

    Users and tokens tables in DB

--- install/assets/install.sql
+++ install/assets/install.sql
@@ -1,4 +1,34 @@
 --
--- Paste your SQL dump into this file
+-- Struttura della tabella `tokens`
 --
 
+CREATE TABLE IF NOT EXISTS `tokens` (
+  `token` varchar(200) NOT NULL,
+  `token_expire` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `client_secret_uuid` varchar(36) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`token`,`client_secret_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Tokens table (Auth for API)';
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Users Table' AUTO_INCREMENT=2 ;
+
+--
+-- Dump dei dati per la tabella `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `lastname`) VALUES
+(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'System', 'Administrator');
